3accbaee3aed3fd8d8d07e3b9a9c5b7dea670a5f
seph <barca014@gmail.com>
2022-06-17 15:50:01 +0100

    sync with master

---
+++ Component/NdiRawSender_Properties.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+namespace Klak.Ndi {
+
+public sealed partial class NdiRawSender : MonoBehaviour {
+    #region NDI source settings
+    [SerializeField] string _ndiName = "NDI Raw Sender";
+    string _ndiNameRuntime;
+
+    public string ndiName
+      { get => _ndiNameRuntime;
+        set => SetNdiName(value); }
+
+    void SetNdiName(string name) {
+        if (_ndiNameRuntime == name) return;
+        _ndiName = _ndiNameRuntime = name;
+        Restart();
+    }
+
+    [SerializeField] bool _keepAlpha = false;
+    [SerializeField] bool _frameUpdated = false;
+
+    public bool keepAlpha { 
+        get => _keepAlpha;
+        set => _keepAlpha = value; 
+    }
+    public bool frameUpdated {
+        get => _frameUpdated;
+        set => _frameUpdated = value;
+    }
+    #endregion
+
+    #region Capture target settings
+    [SerializeField] Texture _sourceTexture = null;
+
+    public Texture sourceTexture
+      { get => _sourceTexture;
+        set => _sourceTexture = value; }
+    #endregion
+
+    #region Runtime property
+    public string metadata { get; set; }
+    public Interop.Send internalSendObject => _send;
+    #endregion
+
+    #region Resources asset reference
+    [SerializeField, HideInInspector] NdiResources _resources = null;
+    public void SetResources(NdiResources resources)
+      => _resources = resources;
+    #endregion
+
+    #region Editor change validation
+    // Applies changes on the serialized fields to the runtime properties.
+    // We use OnValidate on Editor, which also works as an initializer.
+    // Player never call it, so we use Awake instead of it.
+
+    #if UNITY_EDITOR
+    void OnValidate()
+    #else
+    void Awake()
+    #endif
+    {
+        ndiName = _ndiName;
+    }
+    #endregion
+}
+
+} // namespace Klak.Ndi
