dcc9596781534dd63062ca3e1b7c1a285d0c49bd
seph <barca014@gmail.com>
2022-06-21 12:41:50 +0100

    allow for uncompressed data

---
+++ Legacy/Assets/SendTest.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Klak.Ndi;
+
+public class SendTest : MonoBehaviour {
+    protected NdiRawSender _sender;
+    public Texture2D texture;
+
+    void Start() {
+        _sender = FindObjectOfType<NdiRawSender>();
+
+#if true
+        texture = new Texture2D(256, 256, TextureFormat.RGB24, 0, false);
+#else
+        texture = new Texture2D(256, 256, TextureFormat.RGBA32, 0, false);
+#endif
+        Color32[] col = new Color32[256 * 256];
+
+        for (int i = 0; i < 256 * 256; i++) {
+            col[i] = new Color32(
+                (byte)Random.Range(0, 255),
+                (byte)Random.Range(0, 255),
+                (byte)Random.Range(0, 255),
+                (byte)Random.Range(0, 255));
+        }
+        texture.SetPixels32(col);
+        texture.Apply(false, true);
+
+        _sender.sourceTexture = texture;
+    }
+}
